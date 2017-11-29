c2c74e907dd854e266d556ea764e1dd26bcb792f
Vladimir <vgjuran@protonmail.com>
2017-11-29 14:57:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function johnLennonFacts(facts) {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var i = 0;
   while (i < facts.length) {
-    array.push(facts[i] + "!!!");
+    facts[i] += "!!!";
     i++;
   }
   return facts;
