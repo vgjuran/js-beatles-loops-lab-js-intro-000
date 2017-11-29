f55328df7272c1be0afac786fe8ae42ce842a6e2
Vladimir <vgjuran@protonmail.com>
2017-11-29 14:54:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ Use a while loop to loop over the facts array and add "!!!" to the end of every 
 
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 function johnLennonFacts() {
-  
+
   var arr = [];
   var i = 0;
   while (array.length < facts.length) {
