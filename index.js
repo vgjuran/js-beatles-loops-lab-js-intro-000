353cf629eca4ff3262d65b92cea98e5cd49666cc
Vladimir <vgjuran@protonmail.com>
2017-11-29 14:55:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,16 +19,15 @@ const facts = [
 ];
 Use a while loop to loop over the facts array and add "!!!" to the end of every fact. The function should return an array of strings with exclamation points.*/
 
-var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-function johnLennonFacts() {
 
-  var arr = [];
+function johnLennonFacts(facts) {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var i = 0;
-  while (array.length < facts.length) {
+  while (i < facts.length) {
     array.push(facts[i] + "!!!");
     i++;
   }
-  return arr;
+  return facts;
 }
 
 /*Create a function iLoveTheBeatles which accepts a number as a parameter. The body of the function should create a variable that stores an empty array. Then, implement a do-while loop inside the function that adds "I love the Beatles!" to the empty array. Then the loop should increment the number passed in as a parameter. The condition of the loop should check to see that the parameter number is less than 15. The function should return the array with
